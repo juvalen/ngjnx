773a59a07145e47b59d902972d89748ec7178a12
Juan Valentin <juan@verdi>
2019-05-11 12:40:28 +0200

    Vagrantfile.reverse and reverse.pp now WORK

--- manifests/reverse.pp
+++ manifests/reverse.pp
@@ -2,28 +2,8 @@ $docroot = '/var/www/html'
 $packages = ['curl','nginx'] 
 
 package { $packages: 
-   ensure => "installed",
-}
-
-service { 'nginx':
-  ensure => running,
-}
-
-# Set nginx new configuration file
-tidy { "/etc/nginx/sites-enabled/default": }
-tidy { "/etc/nginx/sites-available/default": }
-
-## Create symlink to set reverse-proxy configuration available to nginx
-file { "/etc/nginx/sites-available/reverse-proxy.conf":
-  ensure => 'link',
-  target => "/vagrant/files/reverse-proxy.conf",
-}
-
-## Create symlink to set reverse-proxy configuration enabled to nginx
-file { "/etc/nginx/sites-enabled/reverse-proxy.conf":
-  ensure => 'link',
-  target => "/vagrant/files/reverse-proxy.conf",
-#  notify  => Service['nginx'],
+  ensure => "installed",
+  notify => File['/etc/nginx/sites-available/reverse-proxy.conf'],
 }
 
 # Creates an index file for testing
@@ -31,3 +11,31 @@ file { "${docroot}/index.html":
   ensure => file,
   content => "<h1>Happy Puppet</h1>",
 }
+
+# Create symlink to set reverse-proxy configuration available to nginx
+file { "/etc/nginx/sites-available/reverse-proxy.conf":
+  ensure => 'link',
+  target => "/vagrant/files/reverse-proxy.conf",
+  notify => File['/etc/nginx/sites-enabled/reverse-proxy.conf'],
+}
+
+# Create symlink to set reverse-proxy configuration enabled to nginx
+file { "/etc/nginx/sites-enabled/reverse-proxy.conf":
+  ensure => 'link',
+  target => "/vagrant/files/reverse-proxy.conf",
+  notify  => Tidy['/etc/nginx/sites-enabled/default'],
+}
+
+# Remove nginx default configuration file
+tidy { "/etc/nginx/sites-enabled/default":
+  notify  => Tidy['/etc/nginx/sites-available/default'],
+}
+
+tidy { "/etc/nginx/sites-available/default":
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => "running",
+}
+
