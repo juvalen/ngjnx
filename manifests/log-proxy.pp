5ddb175400105077e4437d1d8cc806a531344182
Juan Valentin <juan@verdi>
2019-05-11 19:36:34 +0200

    Privoxy works in logproxy but only locally

--- manifests/log-proxy.pp
+++ manifests/log-proxy.pp
@@ -8,15 +8,16 @@ package { $packages:
 file { "/etc/privoxy/config":
   ensure => 'file',
   target => "/vagrant/files/config",
-  notify => Exec[listen-address],
+  notify => Service[privoxy],
+#  notify => Exec[listen-address],
 }
 
-exec { 'listen-address':
-  command => "listen-address-enp0s8",
-  path => "/vagrant/files/",
-  user => "root",
-  notify => Service[privoxy],
-}
+#exec { 'listen-address':
+#  command => "listen-address-enp0s8",
+#  path => "/vagrant/files/",
+#  user => "root",
+#  notify => Service[privoxy],
+#}
 
 service { 'privoxy':
   ensure => "running",
